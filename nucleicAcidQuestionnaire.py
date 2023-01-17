1b3ec8e0449f0c6d5e9acfd4848cacf974f490d6
ou guangye <3250679635@qq.com>
2023-01-17 18:55:41 +0800

    加入了配置文件读取信息

--- nucleicAcidQuestionnaire.py
+++ nucleicAcidQuestionnaire.py
@@ -1,14 +1,25 @@
-import time
 from time import sleep
+import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-options = webdriver.ChromeOptions()
 
-driverFile_path = r'D:\application\edgedriver_win64\msedgedriver.exe'
+# 读取配置文件的信息
+config = configparser.ConfigParser()
+config.read('config.ini',encoding="utf-8")
+
+
+# 忽略无用的日志
+options = webdriver.EdgeOptions()
+options.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+
+# 这里可以选择自己合适的浏览器驱动， 我这里选择的是Edge Driver
+driverFile_path = config.get('info','broswersDriverPath')
+print("path", driverFile_path)
 driver = webdriver.Edge(executable_path=driverFile_path)
-# 这行代码的作用是将webdriver这个属性置为undefined
+
+# 这行代码的作用是将webdriver这个属性置为undefined， 避免验证失败
 driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument',
                        {'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'}
                        )
@@ -17,39 +28,52 @@ driver.get(r'https://www.wjx.top/vm/hJzQinm.aspx')
 driver.implicitly_wait(10)
 driver.maximize_window()
 
-actions = webdriver.ActionChains(driver)
+# 输入姓名
+name = driver.find_element(by=By.NAME, value="q1")
+name.send_keys(config.get('info','name'))
 
-input1 = driver.find_element(by=By.NAME, value="q1")
-input1.send_keys('欧光业')
+# 选择类别， 1： 本科生
+type = Select(driver.find_element(by=By.NAME, value='q3'))
+type.select_by_index(int(config.get('info','type')))
 
-for i in range(3, 5):
-    my_input = Select(driver.find_element(by=By.NAME, value='q' + str(i)))
-    my_input.select_by_index(1)
 
-input5 = driver.find_element(by=By.NAME, value='q5')
-input5.send_keys('202030430134')
+# 选择常住校区
+loc = Select(driver.find_element(by=By.NAME, value='q3'))
+loc.select_by_index(int(config.get('info','residentCampus')))
 
+
+# 输入学号
+student_number = driver.find_element(by=By.NAME, value='q5')
+student_number.send_keys(config.get('info','studentNumber'))
+
+# 输入手机号
 input5 = driver.find_element(by=By.NAME, value='q6')
-input5.send_keys('13414970411')
+input5.send_keys(config.get('info','phoneNumber'))
 
+
+'''
+    选择单位, 点击之后会出现弹窗，选择 单位类型 和  单位部门， 最后点击确认按钮
+'''
 input2 = driver.find_element(by=By.NAME, value="q2")
 input2.click()
 
-time.sleep(1)
+# 等待 弹窗出来
+sleep(1)
 
-iframe = driver.find_element(by=By.ID, value='layui-layer1')
-# driver.switch_to.frame(iframe)
-
-input2_hidden1 = driver.find_element(by=By.XPATH,
+# 选择单位类型 2： 院系
+organization_type = driver.find_element(by=By.XPATH,
                                      value="/html/body/div[3]/div[2]/div/div/div[1]/div/select")
-Select(input2_hidden1).select_by_index(2)
+Select(organization_type).select_by_index(int(config.get('info','organizationType')))
 
-input2_hidden2 = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div[2]/div/div/div[2]/div/select')
-Select(input2_hidden2).select_by_index(15)
+
+# 选择单位名称， 15: 计算机科学与工程学院
+organization_name = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div[2]/div/div/div[2]/div/select')
+Select(organization_name).select_by_index(int(config.get('info','organizationName')))
 
 input2_hidden_button = driver.find_element(by=By.CLASS_NAME, value='button_a')
 input2_hidden_button.click()
 
+# 点击提交按钮
 submit_button = driver.find_element(by=By.ID, value='ctlNext')
 submit_button.click()
 
@@ -57,10 +81,8 @@ submit_button.click()
 sleep(1)
 test_button = driver.find_element(by=By.CLASS_NAME, value='layui-layer-btn0')
 test_button.click()
-
 verify_button = driver.find_element(by=By.ID, value='SM_BTN_WRAPPER_1')
 verify_button.click()
 
-# sleep(3)
-
-# driver.quit()
+sleep(3)
+driver.quit()
